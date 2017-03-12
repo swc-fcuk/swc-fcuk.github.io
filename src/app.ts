cca835acfa3bd7b2498b9350a671b8b53c60c511
joshuatee <joshua.j.tee@outlook.com>
2017-03-13 00:38:39 +0200

    Added React

--- src/app.ts
+++
@@ -1,8 +0,0 @@
-import greeting from "./some_module";
-import Greeter from "./greeter";
-
-console.log(greeting);
-
-var greeter = new Greeter("Hello, world!!!");
-const greet = document.body.innerHTML = greeter.greet();
-
