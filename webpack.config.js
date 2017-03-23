6bbe6ada21a9676418ed6efa94158e8a0a83245f
joshuatee <joshua.j.tee@outlook.com>
2017-03-23 10:45:12 +0200

    Add WinJS and a WinJS template

--- webpack.config.js
+++ webpack.config.js
@@ -1,48 +1,30 @@
-var path = require("path");
+var path = require('path');
 
-var config = {
-  watch: true,
-  /*
-   * app.ts represents the entry point to your web application. Webpack will
-   * recursively go through every "require" statement in app.ts and
-   * efficiently build out the application's dependency tree.
-   */
-  entry: ["./src/app.tsx"],
-
-  /*
-   * The combination of path and filename tells Webpack what name to give to
-   * the final bundled JavaScript file and where to store this file.
-   */
+module.exports = {
+  cache: true,
+  entry: './index.jsx',
   output: {
-    path: path.resolve(__dirname, "build"),
-    filename: "bundle.js"
+    filename: 'browser-bundle.js'
   },
-
-  /*
-   * resolve lets Webpack now in advance what file extensions you plan on
-   * "require"ing into the web application, and allows you to drop them
-   * in your code.
-   */
-  resolve: {
-    extensions: [".ts", ".tsx", ".js"]
-  },
-
   module: {
-    /*
-     * Each loader needs an associated Regex test that goes through each
-     * of the files you've included (or in this case, all files but the
-     * ones in the excluded directories) and finds all files that pass
-     * the test. Then it will apply the loader to that file. I haven't
-     * installed ts-loader yet, but will do that shortly.
-     */
     loaders: [
       {
-        test: /\.tsx?$/,
-        loader: "ts-loader",
-        exclude: /node_modules/
+        test: /\.jsx/,
+        loader: 'jsx-loader'
       }
     ]
+  },
+  resolve: {
+    //extensions: ['.js', '.ts', '.tsx'],
+    //// Fix webpack's default behavior to not load packages with jsnext:main module
+    //// (jsnext:main directs not usually distributable es6 format, but es6 sources)
+    //mainFields: ['module', 'browser', 'main']
+    ////, alias: {
+    ////  'react-winjs': path.join(__dirname, '../../react-winjs')
+    ////}
+  },
+  externals: {
+    'react': 'React',
+    'react-dom': 'ReactDOM'
   }
 };
-
-module.exports = config;
