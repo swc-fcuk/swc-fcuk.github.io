3e24e1014b190c2e504a714f05a3bce650b39971
joshuatee <joshua.j.tee@outlook.com>
2017-04-05 23:49:52 +0300

    .

--- src/Data.ts
+++ src/Data.ts
@@ -1,42 +1,48 @@
 import { Squad, SquadMember } from './Squad';
 
-var squad: Squad = require('./../data/568c7e7c-9ddd-11e6-abea-06c66a004f76.json');
+var squad: Squad = require( './568c7e7c-9ddd-11e6-abea-06c66a004f76.json' );
 
 var posterWidth = 400;
 var posterHeight = 400;
 var _canvas: HTMLCanvasElement;
-function makePoster(color: any) {
-    if (!_canvas) {
-        _canvas = document.createElement("canvas");
+function makePoster( color: any )
+{
+    if ( !_canvas )
+    {
+        _canvas = document.createElement( "canvas" );
         _canvas.width = posterWidth;
         _canvas.height = posterHeight;
     }
-    var ctxt = _canvas.getContext("2d");
+    var ctxt = _canvas.getContext( "2d" );
     ctxt.fillStyle = color;
-    ctxt.fillRect(0, 0, posterWidth, posterHeight);
+    ctxt.fillRect( 0, 0, posterWidth, posterHeight );
     return _canvas.toDataURL();
 }
 
 var posterColors = [
-    [68, 34, 87], [100, 66, 119], [132, 98, 151],
-    [164, 162, 165], [196, 194, 197], [228, 226, 229],
-    [220, 77, 6], [252, 109, 38], [255, 141, 70]
+    [ 68, 34, 87 ], [ 100, 66, 119 ], [ 132, 98, 151 ],
+    [ 164, 162, 165 ], [ 196, 194, 197 ], [ 228, 226, 229 ],
+    [ 220, 77, 6 ], [ 252, 109, 38 ], [ 255, 141, 70 ]
 ];
-var posters = posterColors.map(function (color) {
-    return makePoster("rgb(" + color.join(", ") + ")");
-});
+var posters = posterColors.map( function ( color )
+{
+    return makePoster( "rgb(" + color.join( ", " ) + ")" );
+} );
 
-function randomInt(first: any, last: any) {
-    return Math.round(Math.random() * (last - first)) + first;
+function randomInt( first: any, last: any )
+{
+    return Math.round( Math.random() * ( last - first ) ) + first;
 }
 
-function randomElement(array: Array<any>) {
-    return array[randomInt(0, array.length - 1)];
+function randomElement( array: Array<any> )
+{
+    return array[ randomInt( 0, array.length - 1 ) ];
 }
 
 var people: Array<any> = []
 var person: Object
-squad.members.forEach(element => {
+squad.members.forEach( element =>
+{
     person = {
         attacks: element.attacksWon,
         defenses: element.defensesWon,
@@ -59,10 +65,10 @@ squad.members.forEach(element => {
         troopsReceived: element.troopsReceived,
         warParty: element.warParty,
         xp: element.xp,
-        picture: randomElement(posters)
+        picture: randomElement( posters )
     }
-    people.push(person)
-});
+    people.push( person )
+} );
 
 module.exports = {
     people: people,
